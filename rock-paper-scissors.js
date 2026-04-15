de43079f506a8501fd80a3a2e526bad7d8e8b952
Gumba <jeromontz@hotmail.de>
2026-04-15 16:42:33 +0200

    add rock paper scissors game logic

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,52 @@
-console.log('# Output: You chose rock. Computer chose scissors. You win!')
+const choices = ["rock", "paper", "scissors"];
+const computerChoice = choices[Math.floor(Math.random() * 3)];
+const playerChoice = process.argv.slice(2)[0];
+
+if (!choices.includes(playerChoice)) {
+    console.log(
+        "Wrong Input! Please type one of the following: rock, paper, scissors",
+    );
+    return;
+}
+
+const result = getPlayerResult(playerChoice, computerChoice);
+printResult(result, playerChoice, computerChoice)
+
+
+function getPlayerResult(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        return "draw";
+    } else if (
+        (playerChoice === "rock" && computerChoice === "scissors") ||
+        (playerChoice === "paper" && computerChoice === "rock") ||
+        (playerChoice === "scissors" && computerChoice === "paper")
+    ) {
+        return "win";
+    } else {
+        return "lose";
+    }
+}
+
+function printResult(result, playerChoice, computerChoice) {
+    let resultMessage = "";
+    switch (result) {
+        case "draw":
+            resultMessage = "Its a draw!";
+            break;
+        case "win":
+            resultMessage = "You win!";
+            break;
+        case "lose":
+            resultMessage = "You lose!";
+            break;
+    }
+    console.log(
+        "# Output: You chose " +
+            playerChoice +
+            ". Computer chose " +
+            computerChoice +
+            ". " +
+            resultMessage,
+    );
+}
+
